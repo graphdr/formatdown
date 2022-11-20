e8f9b06bd0df8aa3cbadadeaddfaf3bb6d884fa4
graphdr <graphdoctor@gmail.com>
2022-11-19 19:42:39 -0500

    water data

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Ideal gas sample data frame
+#' Air as an ideal gas
 #'
 #' Air density at room temperature. A data frame to illustrate features of
 #' the \code{format_power()}.
@@ -16,3 +16,21 @@
 #'  \item{density}{Numeric air density in kg/m^3}
 #' }
 "density"
+
+
+#' Water properties
+#'
+#' Properties of water at different temperatures. A data frame to illustrate
+#' features of the \code{format_power()}.
+#'
+#' @usage data(water, package = "formatdown")
+#'
+#' @format A data frame with 5 rows and 5 columns:
+#' \describe{
+#'  \item{temperature}{Numeric. K.}
+#'  \item{density}{Numeric. kg/m^3.}
+#'  \item{specific_weight}{Numeric. }
+#'  \item{viscosity}{Numeric. Pa-s.}
+#'  \item{bulk_modulus}{Numeric. Pa.}
+#' }
+"water"
